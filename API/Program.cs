df779ec7756db1a5d02b16fe05b36c8c81482cfd
Elmedin Llumnica <elmedinllumnica@gmail.com>
2026-01-14 10:48:14 +0100

    feat : basic project file configs

--- API/Program.cs
+++ API/Program.cs
@@ -9,14 +9,6 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
