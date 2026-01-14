0a6e96fdd5b5d7d0e6c77b7768683c8647725c9b
Elmedin Llumnica <elmedinllumnica@gmail.com>
2026-01-14 16:21:59 +0100

    feat : made API's async

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -17,16 +18,16 @@ namespace API.Controllers
 
 
         [HttpGet]
-        public ActionResult<List<Product>> GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return context.Products.ToList();
+            return await context.Products.ToListAsync();
 
         }
 
         [HttpGet("{id}")] //api/products/2
-        public ActionResult<Product> GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = context.Products.Find(id);
+            var product = await context.Products.FindAsync(id);
 
             if (product is null) return NotFound();
 
