b57ade517c7a75130bfe203bb1cc743c5f2c8c5d
Elmedin Llumnica <elmedinllumnica@gmail.com>
2026-02-03 12:12:46 +0100

    feat: set up RTK Query base API and store

---
+++ kt-client/src/features/catalog/catalogApi.ts
@@ -0,0 +1,21 @@
+import {createApi} from "@reduxjs/toolkit/query/react"
+import type { Product } from "../../app/models/product"
+import { baseQueryWithErrorHandling } from "../../app/api/baseApi";
+
+
+export const catalogApi = createApi({
+    reducerPath : 'catalogApi',
+    baseQuery : baseQueryWithErrorHandling,
+    endpoints : (builder) =>({
+        fetchProducts : builder.query<Product [] , void>({
+            query : () => ({url : 'products'})
+        }),
+        fetchProductsDetails : builder.query<Product , number>({
+            query : (productsId) => `products/${productsId}`
+        })
+    })
+})
+
+
+export const {useFetchProductsDetailsQuery , useFetchProductsQuery} = catalogApi;
+
